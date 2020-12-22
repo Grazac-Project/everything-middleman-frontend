f8f20d42c487c5034b28d1773d313a150cc4ecb1
mayowa dabiri <mayowad43@gmai.com>
2020-12-22 13:03:46 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./containers/home";
 import Layout from "./hoc/layout";
-import { lazy, Suspense } from "react";
+import Contact from "./containers/contact";
+import About from "./containers/about";
+import Faq from "./containers/faq";
 
-const About = lazy(() => {
-  return import("./containers/about");
-});
+// const About = lazy(() => {
+//   return import("./containers/about");
+// });
 
-const Contact = lazy(() => {
-  return import("./containers/contact");
-});
+// const Contact = lazy(() => {
+//   return import("./containers/contact");
+// });
 
-const Faq = lazy(() => {
-  return import("./containers/faq");
-});
+// const Faq = lazy(() => {
+//   return import("./containers/faq");
+// });
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/sayHi" component={Contact} />
-            <Route path="/about" component={About} />
-            <Route path="/faq" component={Faq} />
-            <Route path="*">
-              <Redirect to="/" />
-            </Route>
-          </Switch>
-        </Suspense>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+          <Route path="/faq" component={Faq} />
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
